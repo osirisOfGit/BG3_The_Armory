3d46f24aa0ded16aec4ca84667b680afb701679a
osirisofgit <github.com.j95fw@passmail.net>
2024-12-31 16:40:52 -0600

    cleaning up

--- TheArmory/Mods/Attunement/ScriptExtender/Lua/BootstrapClient.lua
+++ TheArmory/Mods/Attunement/ScriptExtender/Lua/BootstrapClient.lua
@@ -11,24 +11,3 @@ Ext.Require("Shared/Translator.lua")
 Ext.Require("Shared/Configurations/_ConfigurationStructure.lua")
 
 ConfigurationStructure:InitializeConfig()
-
-Ext.Require("Client/ItemConfig/ItemConfigMenu.lua")
-Ext.Require("Client/Rules/RulesMenu.lua")
-
-Ext.Events.StatsLoaded:Subscribe(function(e)
-	---@type SpellData
-	local isAttunedStat = Ext.Stats.Get("ATTUNEMENT_IS_ATTUNED_STATUS")
-	if MCM.Get("enabled") then
-		isAttunedStat.StatusPropertyFlags = {}
-	else
-		isAttunedStat.StatusPropertyFlags = { "DisableCombatlog", "DisablePortraitIndicator" }
-	end
-
-	---@type SpellData
-	local requiresAttuningStat = Ext.Stats.Get("ATTUNEMENT_REQUIRES_ATTUNEMENT_STATUS")
-	if MCM.Get("enabled") then
-		requiresAttuningStat.StatusPropertyFlags = { "DisableOverhead", "DisableCombatlog" }
-	else
-		requiresAttuningStat.StatusPropertyFlags = { "DisableCombatlog", "DisablePortraitIndicator" }
-	end
-end)
