cdd05d0d8daf28d39ae1608bf3a5467ffff8b68d
osirisofgit <github.com.j95fw@passmail.net>
2024-12-31 20:38:03 -0600

    lookin good

--- The Armory/Mods/The Armory/ScriptExtender/Lua/Client/Vanity/Main.lua
+++ The Armory/Mods/The Armory/ScriptExtender/Lua/Client/Vanity/Main.lua
@@ -4,14 +4,14 @@ Mods.BG3MCM.IMGUIAPI:InsertModMenuTab(ModuleUUID, "Vanity",
 		tabHeader.TextWrapPos = 0
 
 		--#region Presets
-		tabHeader:AddText("Select a Preset").PositionOffset = { 400, 0 }
+		tabHeader:AddText("Select a Preset").PositionOffset = { 200, 0 }
 		local presetCombo = tabHeader:AddCombo("")
 		presetCombo.SameLine = true
 		presetCombo.WidthFitPreview = true
 		presetCombo.Options = { "Preset", "Preset", "Preset" }
 
 		local copyPresetButton = tabHeader:AddButton("Duplicate")
-		copyPresetButton.PositionOffset = { 300, 0 }
+		copyPresetButton.PositionOffset = { 100, 0 }
 
 		local previewPresetButton = tabHeader:AddButton("Preview")
 		previewPresetButton.SameLine = true
@@ -23,20 +23,50 @@ Mods.BG3MCM.IMGUIAPI:InsertModMenuTab(ModuleUUID, "Vanity",
 		--#endregion
 
 		--#region Character Panel
+		tabHeader:AddSeparator()
+
 		local panelGroup = tabHeader:AddGroup("CharacterPanel")
-		panelGroup:AddImageButton("Helmet", "c_slot_helmet")
-		panelGroup:AddImageButton("Cloak", "c_slot_cloak")
-		panelGroup:AddImageButton("Breast", "c_slot_breast")
-		panelGroup:AddImageButton("Gloves", "c_slot_gloves")
-		panelGroup:AddImageButton("Boots", "c_slot_boots")
-		panelGroup:AddImageButton("LightSource", "c_slot_lightSource")
-		panelGroup:AddImageButton("Instrument", "c_slot_instrument")
-		panelGroup:AddImageButton("MeleeMainHand", "c_slot_meleeMainHand")
-		panelGroup:AddImageButton("MeleeOffHand", "c_slot_meleeOffHand")
-		panelGroup:AddImageButton("RangedMainHand", "c_slot_rangedMainHand")
-		panelGroup:AddImageButton("RangedOffHand", "c_slot_rangedOffHand")
-		panelGroup:AddImageButton("VanityClothes", "c_slot_vanityClothes")
-		panelGroup:AddImageButton("VanityBoots", "c_slot_vanityBoots")
+		local slotButtons = {
+			{ "Helmet",         "c_slot_helmet" },
+			{ "VanityClothes",  "c_slot_vanityClothes",  true },
+			{ "Cloak",          "c_slot_cloak" },
+			{ "VanityBoots",    "c_slot_vanityBoots",    true },
+			{ "Breast",         "c_slot_breast" },
+			{ "Underwear",      "c_slot_underwear",      true },
+			{ "Gloves",         "c_slot_gloves" },
+			{ "Amulet",         "c_slot_necklace",       true },
+			{ "Boots",          "c_slot_boots" },
+			{ "Ring1",          "c_slot_ring1",          true },
+			{ "Dummy",          "ignore" },
+			{ "Ring2",          "c_slot_ring2",          true },
+			{ "LightSource",    "c_slot_lightSource" },
+			{ "Instrument",     "c_slot_instrument",     true },
+			{ "MeleeMainHand",  "c_slot_meleeMainHand" },
+			{ "MeleeOffHand",   "c_slot_meleeOffHand",   true },
+			{ "RangedMainHand", "c_slot_rangedMainHand", true },
+			{ "RangedOffHand",  "c_slot_rangedOffHand",  true }
+		}
+
+		for _, button in ipairs(slotButtons) do
+			local imageButton
+			if button[1] == "Dummy" then
+				imageButton = panelGroup:AddDummy(60, 60)
+				imageButton.Label = button[2]
+			else
+				imageButton = panelGroup:AddImageButton(button[1], button[2])
+
+				---@cast imageButton ExtuiImageButton
+
+				if button[1] == "MeleeMainHand" or not button[1]:find("Hand") then
+					imageButton.PositionOffset = { button[1] == "Ring2" and 460 or (imageButton.SameLine and 100 or 200), 0 }
+				end
+
+				panelGroup:AddImageButton(button[1] .. "_Dye", "Item_LOOT_Dye_Remover", { 32, 32 }).SameLine = true
+			end
+			if button[3] then
+				imageButton.SameLine = true
+			end
+		end
 
 		--#endregion
 	end)
