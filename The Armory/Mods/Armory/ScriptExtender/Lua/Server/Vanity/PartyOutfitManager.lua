9a229b50bec0d0a9cb013c262df14379fd259e14
osirisofgit <github.com.j95fw@passmail.net>
2025-01-19 13:19:56 -0600

    optimize outfits a tiny bit more

--- The Armory/Mods/Armory/ScriptExtender/Lua/Server/Vanity/PartyOutfitManager.lua
+++ The Armory/Mods/Armory/ScriptExtender/Lua/Server/Vanity/PartyOutfitManager.lua
@@ -27,6 +27,75 @@ Ext.Events.SessionLoaded:Subscribe(function(e)
 	ActiveVanityPreset = ConfigurationStructure:UpdateConfigForServer().vanity.presets[Ext.Vars.GetModVariables(ModuleUUID).ActivePreset]
 end)
 
+---@param player string
+---@param activeOutfits {[VanityCriteriaCompositeKey] : VanityOutfit}
+local function FindAndApplyOutfit(player, activeOutfits)
+	local startTime = Ext.Utils.MonotonicTime()
+	---@type {[VanityCharacterCriteriaType] : string}
+	local criteriaTable = {}
+
+	---@type EntityHandle
+	local playerEntity = Ext.Entity.Get(player)
+
+	criteriaTable["Race"] = playerEntity.CharacterCreationStats.Race
+	if playerEntity.CharacterCreationStats.SubRace ~= "00000000-0000-0000-0000-000000000000" then
+		criteriaTable["Subrace"] = playerEntity.CharacterCreationStats.SubRace
+	end
+
+	local highestClassLevel = 0
+	for _, classInfo in pairs(playerEntity.Classes.Classes) do
+		if classInfo.Level > highestClassLevel then -- TODO - handle equal case
+			highestClassLevel = classInfo.Level
+			criteriaTable["Class"] = classInfo.ClassUUID
+			if classInfo.SubClassUUID ~= "00000000-0000-0000-0000-000000000000" then
+				criteriaTable["Subclass"] = classInfo.SubClassUUID
+			else
+				criteriaTable["Subclass"] = nil
+			end
+		end
+	end
+
+	--[[
+		 BodyType    BodyShape
+	1       1            0
+	2       1            1
+	3       0            0
+	4       0            1
+	]]
+	local bodyType = playerEntity.CharacterCreationStats.BodyType
+	local bodyShape = playerEntity.CharacterCreationStats.BodyShape
+	bodyShape = bodyType == 0 and bodyShape + 1 or bodyShape
+	bodyType = bodyType == 0 and 2 or bodyType
+	criteriaTable["BodyType"] = bodyShape + bodyType
+
+	---@type ResourceOrigin
+	local originResource = Ext.StaticData.Get(playerEntity.Origin.field_18, "Origin")
+
+	criteriaTable[originResource.IsHenchman and "Hireling" or "Origin"] = originResource.ResourceUUID
+
+	Logger:BasicDebug("Player %s Criteria Table is: \n%s", player, Ext.Json.Stringify(criteriaTable))
+
+	---@type {[ActualSlot]: VanityOutfitSlot}
+	local playerOutfit
+
+	local compositeKey = CreateCriteriaCompositeKey(criteriaTable)
+	if activeOutfits[compositeKey] then
+		playerOutfit = activeOutfits[compositeKey]
+		Logger:BasicDebug("Player %s was matched to an outfit with criteria table: %s", player, Ext.Json.Stringify(criteriaTable))
+	else
+		playerOutfit, compositeKey = OutfitMatcher.findBestMatch(criteriaTable, activeOutfits)
+	end
+
+	if playerOutfit then
+		Logger:BasicDebug("Player %s was matched to an outfit (in %dms) with criteria table: %s", player, Ext.Utils.MonotonicTime() - startTime,
+			Ext.Json.Stringify(ParseCriteriaCompositeKey(compositeKey)))
+		playerEntity.Vars.TheArmory_Vanity_ActiveOutfit = compositeKey
+		Transmogger:MogCharacter(playerEntity)
+	else
+		Logger:BasicInfo("Could not find an outfit for player %s with criteriaTable %s", player, Ext.Json.Stringify(criteriaTable))
+	end
+end
+
 local function ApplyTransmogsPerPreset()
 	local config = ConfigurationStructure:UpdateConfigForServer()
 	local activePresetId = Ext.Vars.GetModVariables(ModuleUUID).ActivePreset
@@ -39,70 +108,7 @@ local function ApplyTransmogsPerPreset()
 
 		if next(activeOutfits) then
 			for _, player in pairs(Osi.DB_Players:Get(nil)) do
-				local startTime = Ext.Utils.MonotonicTime()
-				---@type {[VanityCharacterCriteriaType] : string}
-				local criteriaTable = {}
-
-				---@type EntityHandle
-				local playerEntity = Ext.Entity.Get(player[1])
-
-				criteriaTable["Race"] = playerEntity.CharacterCreationStats.Race
-				if playerEntity.CharacterCreationStats.SubRace ~= "00000000-0000-0000-0000-000000000000" then
-					criteriaTable["Subrace"] = playerEntity.CharacterCreationStats.SubRace
-				end
-
-				local highestClassLevel = 0
-				for _, classInfo in pairs(playerEntity.Classes.Classes) do
-					if classInfo.Level > highestClassLevel then -- TODO - handle equal case
-						highestClassLevel = classInfo.Level
-						criteriaTable["Class"] = classInfo.ClassUUID
-						if classInfo.SubClassUUID ~= "00000000-0000-0000-0000-000000000000" then
-							criteriaTable["Subclass"] = classInfo.SubClassUUID
-						else
-							criteriaTable["Subclass"] = nil
-						end
-					end
-				end
-
-				--[[
-				     BodyType    BodyShape
-				1       1            0
-				2       1            1
-				3       0            0
-				4       0            1
-				]]
-				local bodyType = playerEntity.CharacterCreationStats.BodyType
-				local bodyShape = playerEntity.CharacterCreationStats.BodyShape
-				bodyShape = bodyType == 0 and bodyShape + 1 or bodyShape
-				bodyType = bodyType == 0 and 2 or bodyType
-				criteriaTable["BodyType"] = bodyShape + bodyType
-
-				---@type ResourceOrigin
-				local originResource = Ext.StaticData.Get(playerEntity.Origin.field_18, "Origin")
-
-				criteriaTable[originResource.IsHenchman and "Hireling" or "Origin"] = originResource.ResourceUUID
-
-				Logger:BasicDebug("Player %s Criteria Table is: \n%s", player[1], Ext.Json.Stringify(criteriaTable))
-
-				---@type {[ActualSlot]: VanityOutfitSlot}
-				local playerOutfit
-
-				local compositeKey = CreateCriteriaCompositeKey(criteriaTable)
-				if activeOutfits[compositeKey] then
-					playerOutfit = activeOutfits[compositeKey]
-					Logger:BasicDebug("Player %s was matched to an outfit with criteria table: %s", player[1], Ext.Json.Stringify(criteriaTable))
-				else
-					playerOutfit, compositeKey = OutfitMatcher.findBestMatch(criteriaTable, activeOutfits)
-				end
-
-				if playerOutfit then
-					Logger:BasicDebug("Player %s was matched to an outfit (in %dms) with criteria table: %s", player[1], Ext.Utils.MonotonicTime() - startTime,
-						Ext.Json.Stringify(ParseCriteriaCompositeKey(compositeKey)))
-					playerEntity.Vars.TheArmory_Vanity_ActiveOutfit = compositeKey
-					Transmogger:MogCharacter(playerEntity)
-				else
-					Logger:BasicInfo("Could not find an outfit for player %s with criteriaTable %s", player[1], Ext.Json.Stringify(criteriaTable))
-				end
+				FindAndApplyOutfit(player[1], activeOutfits)
 			end
 		end
 	end
@@ -114,4 +120,10 @@ end)
 
 Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(levelName, isEditorMode)
 	ApplyTransmogsPerPreset()
+end)
+
+Ext.Osiris.RegisterListener("CharacterJoinedParty", 1, "after", function(character)
+	if ActiveVanityPreset and next(ActiveVanityPreset.Outfits) then
+		FindAndApplyOutfit(character, ActiveVanityPreset.Outfits)
+	end
 end)
